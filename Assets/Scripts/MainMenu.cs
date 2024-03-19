6a48d2fc94176dd4e7e5f436c7977ac29e91e5b2
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-03-19 18:48:23 -0500

    UI Menu Implemented

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        gameOver.SetActive(false);
+    }
+
+    public void startgame()
+    { mainMenu.SetActive(false);
+      gameOver.SetActive(true);
+       
+    }
+
+}
